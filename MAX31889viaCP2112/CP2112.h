2a60ec4229df562652d719f20638b02689d8b678
ccoakley <ccoakley@dimerinstruments.com>
2024-09-20 11:32:46 -0700

    Add project files.

---
+++ MAX31889viaCP2112/CP2112.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <cstdint>
+#include "SLABCP2112.h"
+
+import std;
+
+class CP2112
+{
+private:
+	HID_SMBUS_DEVICE device;
+public:
+	int initialize();
+	int read(uint8_t address, uint8_t read_register, uint8_t* data, std::size_t buffer_length, std::size_t num_bytes);
+	int write(uint8_t address, uint8_t* data, std::size_t num_bytes);
+	int cleanup();
+};
+
